98f730b5ddd5e57fe704459f00386834060ed9b7
Salah Eddine <shen.real@live.fr>
2023-10-03 07:19:10 -1200

    done task

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-if ord(c) >= 97 and ord(c) <= 122:
+def islower(c):
+    """Check for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
         return False
-
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for c in str:
+    if ord(c) >= 97 and ord(c) <= 122:
+        c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+        print("")
