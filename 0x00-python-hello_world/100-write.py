53552d6202f1537366a085752fde76bf475e68e9
Salah Eddine <shen.real@live.fr>
2023-10-04 04:34:20 -1200

    done task

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import sys
-msg = "and that piece of art is useful - Dora Korpar, 2015-10-1"
+msg = "and that piece of art is useful - Dora Korpar, 2015-10-19"
 sys.stderr.write(msg + "\n")
 sys.exit(1)
