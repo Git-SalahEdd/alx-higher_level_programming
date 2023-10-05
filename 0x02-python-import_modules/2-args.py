5fa951c9828845a49568eb86edb0818db3f37c3e
Salah Eddine <shen.real@live.fr>
2023-10-05 06:04:36 -1200

    done task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv) < 2:
-    print("0 arguments.")
-else:
-    print(f"{len(sys.argv)} arguments:")
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("0 arguments.")
+    else:
+        print(f"{len(sys.argv)} arguments:")
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
