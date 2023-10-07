9a02d51916ac8e839985640556cbfb3eb97e37ff
Salah Eddine <shen.real@live.fr>
2023-10-07 06:07:24 -1200

    done task

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    """function that removes all characters c and C from a string."""
+    new_str = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_str += char
+    return new_str
