cceb8ca744cfa61a4d0cca2f37cefbca3ed7004d
Salah Eddine <shen.real@live.fr>
2023-10-07 04:03:13 -1200

    done task

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     """function that replaces an element of a list at a specific position"""
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
     for i in range(len(my_list)):
         if i == idx:
