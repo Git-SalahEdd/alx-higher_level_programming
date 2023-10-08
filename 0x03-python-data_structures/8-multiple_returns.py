b6f1052271519a0fb8aacfc7378b6d86a16612f4
Salah Eddine <shen.real@live.fr>
2023-10-08 06:18:37 -1200

    done task

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    """function that returns a tuple with the length of a string and its first character."""
+    """function that returns a tuple
+    with the length of a string and its first character."""
     sentence_length = len(sentence)
     if not sentence:
         return sentence_length, None
