d425f9f791c855738cbb476aebb82f088616b92e
Salah Eddine <shen.real@live.fr>
2023-10-11 03:28:07 -1200

    done task

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    """function that replaces all occurrences of an element by another in a new list."""
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
+
