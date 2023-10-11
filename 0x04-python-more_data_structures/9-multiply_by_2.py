c670994a70435041ed77ef83994788f1059ed37b
Salah Eddine <shen.real@live.fr>
2023-10-11 06:28:09 -1200

    done task

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    """function that returns a new dictionary with all values multiplied by 2"""
+    """function that returns
+    a new dictionary with all values multiplied by 2"""
     new_dir = a_dictionary.copy()
     list_keys = list(new_dir.keys())
 
