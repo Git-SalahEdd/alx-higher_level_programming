3b703816bae02fc1cd14995b592609d4b552867c
Salah Eddine <shen.real@live.fr>
2023-10-23 02:00:38 -1200

    done task

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    """divides element by element 2 lists."""
+    new = []
+    for i in range(list_length):
+        result = 0
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+        except ZeroDivisionError:
+            print("division by 0")
+        finally:
+            new.append(result)
+    return new
