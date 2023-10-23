4d67f5e6760a0060791787c90ff4c0d4c0fa8f12
Salah Eddine <shen.real@live.fr>
2023-10-23 01:54:12 -1200

    done task

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     """divides 2 integers and prints the result."""
-    result = 0
+    result = None
     try:
         result = a / b
     except ZeroDivisionError:
