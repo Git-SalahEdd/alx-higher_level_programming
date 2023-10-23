bdc3b911444ba9810448a398528b62217387d451
Salah Eddine <shen.real@live.fr>
2023-10-23 02:11:38 -1200

    done task

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    """function that prints an integer."""
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return False
+    else:
+        return True
