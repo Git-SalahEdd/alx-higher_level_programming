9240405a46f996b33fffc2def304e4de07dc5b3c
Salah Eddine <shen.real@live.fr>
2023-10-23 01:52:41 -1200

    done task

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,7 +5,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        continue
+        pass
     finally:
         print("Inside result: {}".format(result))
         return result
