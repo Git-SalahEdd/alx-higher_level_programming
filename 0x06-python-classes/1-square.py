ea2db066abbaffbee2d3101957f30fe3b5b73d35
Salah Eddine <shen.real@live.fr>
2023-10-24 05:19:14 -1200

    done task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""square module."""
+
+
+class Square:
+    """Define a Square"""
+    def __init__(self, size):
+        """Constroctor.
+        Args : 
+            size : lenth of a side of the Square.
+        """
+        self.__size = size
