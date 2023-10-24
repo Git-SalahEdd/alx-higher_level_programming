2c21273f92ea192e3a23ce95904335aacc5dd139
Salah Eddine <shen.real@live.fr>
2023-10-24 08:31:50 -1200

    done task

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -35,7 +35,7 @@ class Square:
             returns the current square area
         """
         return self.__size ** 2
-    
+
     def my_print(self):
         if self.size == 0:
             print()
