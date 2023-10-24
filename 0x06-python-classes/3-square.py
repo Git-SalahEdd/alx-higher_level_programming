7f5082ca0faca994275a92c015ea4b98b3114606
Salah Eddine <shen.real@live.fr>
2023-10-24 05:47:04 -1200

    done task

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Square Module """
+
+
+class Square:
+    """ Define Square """
+    def __init__(self, size=0):
+        """Constroctor.
+
+        Args :
+            size : lenth of a side of the Square.
+
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if not size >= 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+    def area(self):
+        """Constroctor.
+            returns the current square area
+        """
+        return self.__size ** 2
