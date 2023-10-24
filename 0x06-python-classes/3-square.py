0a84dd4ccf8be15839a4420c78d5578dbc81005a
Salah Eddine <shen.real@live.fr>
2023-10-24 05:51:23 -1200

    done task

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -19,6 +19,7 @@ class Square:
         if not size >= 0:
             raise ValueError('size must be >= 0')
         self.__size = size
+
     def area(self):
         """Constroctor.
             returns the current square area
