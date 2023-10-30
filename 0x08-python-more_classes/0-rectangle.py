4fb9bf6f829df1fd865747cad74de78f63a40c7d
Salah Eddine <shen.real@live.fr>
2023-10-30 05:12:00 -1200

    done task

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-""" 
-    Module for empty class Rectangle that defines a rectangle
-"""
+"""Module for empty class Rectangle that defines a rectangle"""
+
 
 class Rectangle:
     pass
