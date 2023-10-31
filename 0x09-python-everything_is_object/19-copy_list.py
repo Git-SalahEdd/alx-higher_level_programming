b9e82f33d93ddce4c647b84a04531ca9acb80297
Salah Eddine <shen.real@live.fr>
2023-10-31 07:07:07 -1200

    done task

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-"""
-   Module function copy_list.
-"""
-
-
 def copy_list(l):
-    """
-        function copy_list that returns a copy of a list.
-    """
-    o = l[:]
-    return o
+    return l[:]
