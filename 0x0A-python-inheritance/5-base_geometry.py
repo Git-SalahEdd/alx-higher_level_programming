ceaecc818cdd994105e9e30ae75f8ebdeda40371
Salah Eddine <shen.real@live.fr>
2023-11-06 04:57:00 -1200

    done task

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module for the class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class
+    """
+
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module for the class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class with public instance method: def area(self)
+    """
+
+    def area(self):
+        """
+        Raises Exception with msg 'area() is not implemented'
+        """
+        raise Exception("area() is not implemented")
