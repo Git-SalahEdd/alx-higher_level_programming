93322b01a3e065170874fee73d3ef48f54c26acb
Salah Eddine <shen.real@live.fr>
2023-11-06 03:36:08 -1200

    done task

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module 0. Lookup """
+
+
+def lookup(obj):
+    """ function that returns the list
+        of available attributes and methods of an object
+        Args:
+            obj: object to return
+            his list of available attributes and methods"""
+    return dir(obj)
