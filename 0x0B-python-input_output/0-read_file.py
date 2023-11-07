ac2489b07ac81772b99142652fba630171773216
Salah Eddine <shen.real@live.fr>
2023-11-07 05:16:09 -1200

    done task

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """function that reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, encoding="utf-8") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         read_file_data = file.read()
-        print(read_file_data)
+        print(read_file_data, end='')
