a5559ac50c2ba298f796224fc158aa15c5bc8177
Salah Eddine <shen.real@live.fr>
2023-11-07 06:16:36 -1200

    done task

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module to adds all arguments to a Python list,
+and then save them to a file
+"""
+from sys import argv
+
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+
+try:
+    exist = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    exist = []
+exist.extend(argv[1:])
+save_to_json_file(exist, "add_item.json")
