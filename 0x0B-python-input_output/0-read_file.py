f789c0be6bbae767c927f7e7eb8f1fd46bc9c182
Salah Eddine <shen.real@live.fr>
2023-11-07 04:56:09 -1200

    done task

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module to reads a text file (UTF8) and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """function that reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, encoding="utf-8") as file:
+        read_file_data = file.read()
+        print(read_file_data)
