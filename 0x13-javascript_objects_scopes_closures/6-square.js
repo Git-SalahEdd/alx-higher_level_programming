e5be57897a5f687f77242b562d4aabca1771fc8a
Salah Eddine <shen.real@live.fr>
2024-01-09 06:53:41 -1200

    done task

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const SquareP = require('./5-square');
+
+class Square extends SquareP {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
