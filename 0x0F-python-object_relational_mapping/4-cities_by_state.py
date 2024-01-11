0138c918ff62e3e6535188011ec0f56fd2f7a6b3
Salah Eddine <shen.real@live.fr>
2024-01-11 05:47:12 -1200

    done task

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A script that lists all cities from the database hbtn_0e_4_usa"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        port=3306
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON states.id = cities.state_id \
+    ORDER BY cities.id")
+    res = cursor.fetchall()
+    for row in res:
+        print(row)
+    cursor.close()
+    db.close()
