078196fcb2a32771434240b691764a8da1d3811b
Salah Eddine <shen.real@live.fr>
2024-01-25 18:35:41 -1200

    done task

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Python script that sends a request to the URL and displays the body. """
+import requests
+import sys
+
+if __name__ == "__main__":
+    response = requests.get(sys.argv[1])
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
